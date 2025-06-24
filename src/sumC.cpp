3e0f0ef2ef454e5a28103b60463d8b2430b290d6
hazelcyril <hazel.cyril@warwick.ac.uk>
2025-06-24 15:29:55 +0100

    Added documention for functions

--- src/sumC.cpp
+++ src/sumC.cpp
@@ -1,6 +1,12 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
+
+//' Adds all numbers in a numeric vector
+//'
+//' Returns the sum of all numbers in a numeric vector
+//' @param x
+//' @export
 // [[Rcpp::export]]
 double sumC(NumericVector x) {
   int n = x.size();
